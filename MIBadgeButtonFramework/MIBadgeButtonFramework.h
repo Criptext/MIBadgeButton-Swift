b525570ae06c5ed0d4e780efdb53ae3c24977289
Gianni Carlo <gcarlo89@hotmail.com>
2018-04-06 12:35:33 -0500

    Carthage update

---
+++ MIBadgeButtonFramework/MIBadgeButtonFramework.h
@@ -0,0 +1,19 @@
+//
+//  MIBadgeButtonFramework.h
+//  MIBadgeButtonFramework
+//
+//  Created by Gianni Carlo on 4/6/18.
+//  Copyright © 2018 Youxel Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MIBadgeButtonFramework.
+FOUNDATION_EXPORT double MIBadgeButtonFrameworkVersionNumber;
+
+//! Project version string for MIBadgeButtonFramework.
+FOUNDATION_EXPORT const unsigned char MIBadgeButtonFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MIBadgeButtonFramework/PublicHeader.h>
+
+
